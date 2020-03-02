963502a60b7f114bbd50cf8f829167eb0017a77f
sdvacap <59981151+sdvacap@users.noreply.github.com>
2020-03-02 16:02:10 -0600

    Create AT_content.py

---
+++ AT_content.py
@@ -0,0 +1,22 @@
+#AT Content
+#Sarah Vaca
+#March 02,2020
+
+#Write a program that will print out the AT content of this DNA sequence:
+#ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT 
+
+#Use normal mathematical symbols like add (+), subtract (-), multiply (*), divide (/) and parentheses to carry out calculations on numbers in Python
+
+###STEP 1
+#create the variable for the sequence
+sequence=("ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT")     
+
+###STEP 2
+#for calculate the As content in the sequence 
+sequence.count("A")   
+#for calculate the Ts content in the sequence                                                  
+sequence.count("T") 
+
+###STEP 3         
+#use "count" to calculate the TA content in the sequence                                                 
+sequence.count("A")+sequence.count("T")                                 
