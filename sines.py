2ce52c4d0a7f03f2b8d3dcd247eb1778d9a6830d
sdvacap <59981151+sdvacap@users.noreply.github.com>
2020-03-18 13:38:13 -0500

    Update sines.py

--- sines.py
+++ sines.py
@@ -1,6 +1,6 @@
 #Sines 
 #Sarah Vaca
-#March 9, 2020
+#March 17, 2020
 
 #1. Use avan_rm.bed file in “/lustre/work/jenjense/python/Pandas”
 #cd /lustre/work/jen
@@ -8,52 +8,56 @@
 #cd /lustre/work/svaca/python
 
 #2. Make sure it has proper column names
-#nano aVan_rm.bed
+#read in my bed file 
+print("Read bed file...")
+bed=pd.read_csv("aVan_rm.bed", sep= "\t")        
 
-#3.  Determine what families are in there (SINE, etc)
 ##import modules
 import pandas as pd
 print("#########################")
-#read in my bed file 
-print("Read bed file...")
-sines=pd.read_bed("aVan_rm.bed")        #read in my bed file 
-print(aVan_rm())
+print("Rename columns...")
+column_names=["Scaffold", "Start", "Stop", "Element", "Score", "Strand", "Family", "Sub-Family", "Divergence"]
+#give name to columns
+bed.column=column_names
+print(bed.head())
 
+#3.  Determine what families are in there (SINE, etc)
 print("#########################")
 print("What families are?")
-sines.Family.unique()
+families=bed.Family.unique()
+print("The families in bedfile are: ", families")
 
 #4.  Create new dataframe from that file using only elements in family “SINE” 
 print("#########################")
 print("New dataframe form family SINEs")
 data={"metulj","ZenoSINE"}
-new_sines=pd.DataFrame(data)
-print(new_sines)
+new_bed=bed[bed["Family"]=="new_bed"]
+print(new_bed.head())
 
 print("#########################")
 #5.  Drop columns “Strand” and “Score”
 print("Drop columns Strand and Score..")
-sines.drop(["Strand”, "Score"], axis=1 )
-print(sines)
+new_bed=new_bed.drop(["Strand”, "Score"], axis=1 )
+print(new_bed.head())
 
 print("#########################")
 #6.  Create new column “Length”
 print("New column "Length")
-length=["127", "341", "123","132"]
-sines["Length"]=length
-print("sines")
+new_bed["Length"]=new_bed["Stop"]-new_bed["Start"]
+print("New column created "Length" between Stop and Start")
+print(new_bed.head())
 
 print("#########################")
 #7.  Determine min, max, and mean for all SINEs
 print("What is the minimum, maximun and mean for all SINEs?")
-print(sines["Family"].min(), sines["Family"].max())
-print(sines["Family"].sum()/len(sines))
-print(sines["Family"].mean())
+print(new_bed["Length"].min())
+print(new_bed["Length"].max())
+print(new_bed["Length"].mean())
 
-
-#print("#########################")
+print("#########################")
 #8.  Determine min, max, and mean length for each sub-family of SINE (metulj and ZenoSINE)
-#print("What is the minimum, maximun and mean length for each sub-family of SINE?")
-#print("sines")
-
-#print("#########################")
+print("What is the minimum, maximun and mean length for each sub-family of SINE?")
+print(new_bed.groupy("Sub-Family")["Length"].min())
+print(new_bed.groupy("Sub-Family")["Length"].max())
+print(new_bed.groupy("Sub-Family")["Length"].mean())
+print("#########################")
