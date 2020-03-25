4c41801a2a9e40cc94078b3166d2c9d741997654
sdvacap <59981151+sdvacap@users.noreply.github.com>
2020-03-25 13:17:54 -0500

    Update average.py

--- average.py
+++ average.py
@@ -8,31 +8,20 @@
 #Calculation of sum
 #Print results
 
-n=input('enter number to calculate sum')
 #enter number to calculate sum 0   #ANSWER
-n_s=int(n)
-print(n_s)
-#0  #ANSWER
 sum=0
 for num in range(1,21):
     sum=sum+num                                        #sum of 0 plus each number in the range(1,21)    
-    print("Sum of first ", n_s, "numbers is: ", sum)   #it mean is going to print the first n      numbers, and the sum of those numbers would be n 
+print("Sum of numbers is: ", sum)   
 #Sum of numbers from 1 to 20 = TOTAL 210 ANSWER
 
 #Calculation of average
 #Print results
-
-print ("calculate the average of first n numbers")
-#calculate the average of first n numbers
-n = input("Enter Number ")
-#Enter Number 20
-n = int (n)
-average = 0
 sum = 0
 for num in range(1,21):
     sum = sum+num;
-    average = sum / n
-    print("Average of first ", n, "numbers is: ", average)
+    average = sum / 20
+print("Average is: ", average)
 
 
 #Part 2
@@ -42,7 +31,5 @@ for num in range(1,21):
 
 import statistics
 
-n=list(range(1,21))     #list of the numbers between 1 and 20
-print(n)
-
-print(statistics.mean(n))       #for calculate the average of the number between 1 an 20
+average=statistics.mean(range(1,21))        #for calculate the average of the number between 1 an 20
+print(average)                              
