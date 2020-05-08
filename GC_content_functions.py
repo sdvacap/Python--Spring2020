7d14a874b3f991022c39d942348e4d8c429065d5
sdvacap <59981151+sdvacap@users.noreply.github.com>
2020-05-08 15:38:17 -0500

    Update GC_content_functions.py

--- GC_content_functions.py
+++ GC_content_functions.py
@@ -23,13 +23,23 @@ print('Small sequence of 100bp long: \n' , small_seq)
 ###3. In a loop, use the GC function you created to calculate the GC content for each smaller sequence
 
 #Function to calculate the GC content
-def GC_calculator(sequence):
-    G=sequence.count("G")
-    C=sequence.count("C")
-    A=sequence.count("A")
-    T=sequence.count("T")
-    G_C=(G+C)/(G+C+A+T)
-    return G_C
+def GC_calculator(small_seq):
+    G_C=((small_seq.count("G"))+(small_seq.count("C")))/100
+    return (G_C * 100)     
+            
+#def GC_calculator(seq):
+#    G=seq.count('G')
+#    C=seq.count('C')
+#    G_C=(G+C)/100
+#    return G_C
+
+#def bp_count(seq, allowed_bases=['G','C']):
+#    seq=seq.upper()
+#    total_dna_bases=0
+#    for base in allowed bases:
+#        total_dna_bases:
+#    dna_fraction=total_dna_bases / len(seq)
+#    return(dna_fraction * 100)
 
 #Function to calculate the GC content for each smaller sequence 
 GC_content=[]
